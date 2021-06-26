adcf1765e72bf1ec3149fb5eb863e7a2562dea4f
Sherman Ng <shermannws@gmail.com>
2021-06-26 11:14:39 +0800

    added eventscontext back

---
+++ src/contexts/EventsContext.js
@@ -0,0 +1,28 @@
+import React, { useContext, useState, useEffect } from "react"
+// import { db } from "../firebase"
+// import firebase from "firebase/app"
+
+const EventsContext = React.createContext()
+
+export function useEvents() {
+  return useContext(EventsContext)
+}
+
+export function EventsProvider({ children }) {
+  const [currentGroup, setCurrentGroup] = useState(JSON.parse(localStorage.getItem("group")))
+
+  useEffect(() => {
+    localStorage.setItem("group", JSON.stringify(currentGroup))
+  }, [currentGroup])
+
+  const value = {
+    currentGroup,
+    setCurrentGroup
+  }
+
+  return (
+    <EventsContext.Provider value={value}>
+      {children}
+    </EventsContext.Provider>
+  )
+}
